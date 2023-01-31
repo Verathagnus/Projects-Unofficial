891a3f216c0412585683e77a3e589aa710acebdc
tanveer-98 <tnvrahmed98@gmail.com>
2023-01-31 11:28:13 +0530

    style : UI animation updates to Our Team section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useAnimation,motion } from "framer-motion";
 import {useInView} from 'react-intersection-observer'
 import Hero from './components/hero'
 import AboutUs from './components/aboutus'
+import Team from './components/ourteam'
 function App() {
   const [count, setCount] = useState(0);
   const [open, setOpen] = useState(false);
@@ -299,68 +300,7 @@ function App() {
         </div>
       </section>
 
-      <section id="outTeam">
-        <div className="container mx-auto font-semibold text-center md:text-4xl">
-          <h2 className="mb-6 text-3xl font-semibold text-center md:text-4xl">
-            Our Team
-          </h2>
-        </div>
-        {/* Boxes Container */}
-        <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
-          {/* Box 1 */}
-          <div className="flex flex-col w-full py-6 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-            {/* Image  */}
-            <div className="flex justify-center">
-              <img src="src/assets/logo-chrome.svg" alt="Chrome Image" />
-            </div>
-            <h5 className="pt-6 text-xl font-bold">Bishwaraj Paul</h5>
-            <p className="text-gray-400"> Minimum Version 62</p>
-            {/* Dots */}
-            <div className="bg-dots bg-repeat-x px-6 pt-6 capitalize">
-              <a
-                href="#"
-                className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue"
-              >
-                More
-              </a>
-            </div>
-          </div>
-          <div className="flex flex-col w-full py-6 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-            {/* Image  */}
-            <div className="flex justify-center">
-              <img src="src/assets/logo-chrome.svg" alt="Chrome Image" />
-            </div>
-            <h5 className="pt-6 text-xl font-bold">Tanveer Ahmed</h5>
-            <p className="text-gray-400"> Minimum Version 62</p>
-            {/* Dots */}
-            <div className="bg-dots bg-repeat-x px-6 pt-6 capitalize">
-              <a
-                href="#"
-                className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue"
-              >
-                More
-              </a>
-            </div>
-          </div>
-          <div className="flex flex-col w-full py-6 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
-            {/* Image  */}
-            <div className="flex justify-center">
-              <img src="src/assets/logo-chrome.svg" alt="Chrome Image" />
-            </div>
-            <h5 className="pt-6 text-xl font-bold">Chinmoy Talukdar</h5>
-            <p className="text-gray-400"> Minimum Version 62</p>
-            {/* Dots */}
-            <div className="bg-dots bg-repeat-x px-6 pt-6 capitalize">
-              <a
-                href="#"
-                className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue"
-              >
-                More
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Team/>
       <section id="faq">
         <div className="container mx-auto mt-10 ">
           <h2 className="mb-6 text-3xl font-semibold text-center md:text-4xl">
