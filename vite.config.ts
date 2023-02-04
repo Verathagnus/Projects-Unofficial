2e557fc727b2eff2b404988de97b52acac29d310
Verathagnus <theriseofthedarkknight@gmail.com>
2023-02-04 12:04:09 +0530

    docker nginx config correction

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.png', '**/*.jpeg','**/*.jpg','**/*.svg'],
-  publicDir: './src/assets'
+  publicDir: './src/assets',
 })
